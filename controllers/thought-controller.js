5b45521eef649c48a88884c7307a48f4b739740c
fringemonkey89 <markomajeticjr@gmail.com>
2023-11-08 13:26:10 -0500

    created user and thought controller

---
+++ controllers/thought-controller.js
@@ -0,0 +1,24 @@
+const {User, Thought} = require('../models');
+
+// get all thoughts
+
+
+//get thought by id
+
+
+//create thoughts
+
+
+//update thoughts
+
+
+// delete thought
+
+
+
+
+
+
+
+
+module.exports = thoughtController;
---
+++ controllers/user-controller.js
@@ -0,0 +1,25 @@
+const {User, Thought} = require('../models');
+
+//get all users
+
+
+//get user by id
+
+
+//create user
+
+
+//uodate user
+
+
+
+//delete user
+
+
+//add friend
+
+
+// delete friend
+
+
+module.exports = userController;
