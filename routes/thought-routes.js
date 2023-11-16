1cbeee0a02e3f8656ba5c635e1fd90f3bdfafc5d
fringemonkey89 <markomajeticjr@gmail.com>
2023-11-16 11:44:58 -0500

    added routes to thought routes

--- routes/thought-routes.js
+++ routes/thought-routes.js
@@ -5,7 +5,9 @@ const {
     getThoughtById,
     createThought,
     updateThought,
-    deleteThought
+    deleteThought,
+    addReaction,
+    removeReaction
 } = require('../controllers/thought-controller');
 
 
@@ -14,5 +16,9 @@ router.route('/').get(getAllThoughts)
 router.route('/:userId').post(createThought)
 
 router.route('/:thoughtId').get(getThoughtById).put(updateThought).delete(deleteThought)
+
+router.route('/:thoughtId/reactions/').post(addReaction)
+
+router.route('/:thoughtId/reactions/:reactionId').delete(removeReaction)
 
 module.exports = router;
